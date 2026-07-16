58bbdbd424571774e1502298e5b0c4785965354f
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-16 15:44:56 +0200

    feat: set login system

--- App/Controllers/Auth/LoginController.php
+++ App/Controllers/Auth/LoginController.php
@@ -8,25 +8,45 @@ use Neo\Core\Http\Response\JsonResponse;
 use Neo\Core\Routing\Attribute\MainRoute;
 use Neo\Core\Routing\Attribute\Route;
 use Neo\Core\Http\Response\Response;
+use Neo\Core\Security\Auth\PasswordManager;
 use Neo\Core\Security\Middleware\Attribute\Middleware;
 use Neo\Core\Security\Middleware\Default\GuestMiddleware;
+use Neo\Core\Translation\TranslationManager;
 use Neo\Src\Neo_Admin\Database\Forms\AdministratorForm;
 
 #[Middleware(use: GuestMiddleware::class, redirect: 'default.index')]
 #[MainRoute(path: '/auth/login', name: 'auth.login')]
 final class LoginController extends AbstractController
 {
+    public function __construct(
+        protected TranslationManager $translationManager,
+    ) {
+    }
+
     #[Route(path: '/', name: 'index', methods: ['GET', 'POST'])]
-    public function index(AdministratorForm $administratorForm): Response
+    public function index(AdministratorForm $administratorForm, PasswordManager $passwordManager): Response
     {
         $loginForm = $administratorForm->buildLogin();
 
         if ($loginForm->isSubmitted() && $loginForm->isValid()) {
             $data = $loginForm->getData();
-            $checkAccount = $this->auth()->attempt([
+            $isAuth = $this->auth()->attempt([
                 'username' => $data->getUsername(),
                 'password' => $data->getPassword()
             ]);
+
+            if ($isAuth) {
+                return $this->redirectToRoute('default.index');
+            } else {
+                $this->getFlash()->add(
+                    'error',
+                    $this->translationManager->translate(
+                        'Nom d\'utilisateur et/ou mot de passe invalide',
+                        [],
+                        'auth'
+                    )
+                );
+            }
         }
 
         return $this->render('pages/auth/login/index.html.twig', [
