b1e4f96c42cf3bd3c0f4e745df7ca5e12f209a98
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-16 09:14:24 +0200

    feat: add default DashboardController

---
+++ App/Controllers/Panel/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Neo\Src\Neo_Admin\App\Controllers\Panel;
+
+use Neo\Core\Controller\AbstractController;
+use Neo\Core\Routing\Attribute\MainRoute;
+use Neo\Core\Routing\Attribute\Route;
+use Neo\Core\Http\Response\Response;
+use Neo\Core\Security\Middleware\Attribute\Middleware;
+use Neo\Core\Security\Middleware\Default\AuthMiddleware;
+
+#[Middleware(use: AuthMiddleware::class, redirect: 'default.index')]
+#[MainRoute(path: '/panel/dashboard', name: 'panel.dashboard')]
+final class DashboardController extends AbstractController
+{
+    #[Route(path: '/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->json([
+            'data' => 'Dashboard panel'
+        ]);
+    }
+}
