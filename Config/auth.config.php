cc9706bf6689bd85db0d8abea4de4753be0d1d82
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-07-18 17:08:21 +0200

    fix: align correctly auth config

--- Config/auth.config.php
+++ Config/auth.config.php
@@ -4,25 +4,23 @@ declare(strict_types=1);
 // ./src/Neo_Admin/Config/auth.config.php
 
 return [
-    'auth' => [
-        'enabled'    => true,
-        'model'      => Neo\Src\Neo_Admin\Database\Model\Administrator::class,
-        'identifier' => 'username',
-        'password'   => 'password',
-        'guard'      => 'session',
-        'role'       => [
-            'model'       => Neo\Src\Neo_Admin\Database\Model\AdministratorRole::class,
-            'foreign_key' => 'role_id',
-            'field'       => 'label',
-        ],
-        'options' => [
-            'login'      => '',
-            'logout'     => '',
-            'home'       => '',
-            'secret'     => '',
-            'expiration' => 3600,
-            'timeout'    => 1800,
-            'algorithm'  => 'HS256',
-        ],
+    'enabled'    => true,
+    'model'      => Neo\Src\Neo_Admin\Database\Model\Administrator::class,
+    'identifier' => 'username',
+    'password'   => 'password',
+    'guard'      => 'session',
+    'role'       => [
+        'model'       => Neo\Src\Neo_Admin\Database\Model\AdministratorRole::class,
+        'foreign_key' => 'role_id',
+        'field'       => 'label',
+    ],
+    'options' => [
+        'login'      => '',
+        'logout'     => '',
+        'home'       => '',
+        'secret'     => '',
+        'expiration' => 3600,
+        'timeout'    => 1800,
+        'algorithm'  => 'HS256',
     ],
 ];
